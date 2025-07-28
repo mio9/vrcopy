74d8f303dbba6e51f7d1dd1f336d02750e5cdcb9
Mio Nekoya <admin@mio9.sh>
2025-07-29 06:11:25 +0900

    things can copy now

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
+  <div class="flex justify-center items-center min-h-screen bg-slate-800">
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button class="bg-red-500 text-white px-4 py-2 rounded" @click="copy(text)">Fuck</button>
+    <div v-if="isSupported">
+      <button @click="copy(text)">
+        <!-- by default, `copied` will be reset in 1.5s -->
+        <span v-if="!copied">Copy</span>
+        <span v-else>Copied!</span>
+      </button>
+      <p>Current copied: <code>{{ text || 'none' }}</code></p>
+    </div>
+    <p v-else>
+      Your browser does not support Clipboard API
+    </p>
+    <br />
+    <hr />
+    <div>
+      <Textarea v-model="text">asd</Textarea>
+    </div>
+
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useClipboard } from '@vueuse/core';
+
+const text = ref('')
+const { copy, copied, isSupported } = useClipboard({ source: text })
+
+
+
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['reka-ui/nuxt', '@nuxtjs/tailwindcss'],
+  // css: ['@/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    port: 3890
+  }
 })
