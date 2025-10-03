1a5e17cf792ef0295ef7bbbea199cda4b888c0b5
Mio Nekoya <admin@mio9.sh>
2025-10-04 07:37:51 +0900

    update footer 🌶️

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,7 +1,11 @@
 <template>
     <footer class="mt-auto pt-8 text-center">
-        <div class="text-gray-500 text-sm">
-            VR Copyboard - 2025 MIO9 - MIT "clone this yourself" License
+        <div class="text-gray-500 text-sm relative group cursor-help">
+            VR Copyboard - 2025 MIO9 - MIT License, clone this yourself
+            <div class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-gray-800 text-white text-xs rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10">
+                Would've been some ￥500 shit if made by people on booth <img src="/bonk-stupid.gif" alt="Booth" class="inline-block h-4 w-4"></img>
+                <div class="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-800"></div>
+            </div>
         </div>
     </footer>
 </template>
